ad8538783d76bd9bef118b3477046f4c7844806f
stritch-dev <stritch.dev@gmail.com>
2020-02-19 23:23:50 -0600

    Converted from Java.  All but one tests are passing.

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    
+    
+    public static class StringCalculator
+    {
+          public static int Add(string inputWithDelimiter )
+          {
+              switch (inputWithDelimiter)
+              {
+                  case null:
+                      throw new ArgumentNullException(nameof(inputWithDelimiter));
+                  case "":
+                      { return 0;}
+              }
+
+                  var delimiter = GetDelimiter(inputWithDelimiter);
+              
+                  var numberString = Regex.Replace(inputWithDelimiter, "//", "");
+                  numberString = Regex.Replace(numberString, "[\\[\\]]", "");
+                  var a = numberString.ToCharArray();
+                  if (!char.IsDigit(numberString.ToCharArray()[0]))
+                  {
+                      numberString = numberString.Substring(1);
+                  }
+
+                  var rx = new Regex(
+                      delimiter); // Thanks https://stackoverflow.com/questions/8928601/how-can-i-split-a-string-with-a-string-delimiter
+                  var numbers = rx.Split(numberString);
+
+                  return GetSum(numbers);
+          }
+        
+        
+        public static int GetSum(string[] numberList)
+        {
+            var answer = 0;
+            var error = new StringBuilder();
+
+            foreach (var number in numberList)
+            {
+                var replace = Regex.Replace(number, "\n", "");
+                replace = Regex.Replace(number, "\r", "");
+                var value = int.Parse(replace);
+
+                if (value < 0)
+                {
+                    error.Append($"{value} ");
+                }
+                else if (value < 1001)
+                {
+                    answer += value;
+                    var a = 10;
+                }
+            }
+
+            if (error.Length > 0) throw new ArgumentException("Negatives are not allowed: " + error);
+            return answer;
+        }
+
+        public static string GetDelimiter(string input)
+        {
+            var delimiter = "[,\n\r]";
+            if (input.IndexOf("//[", StringComparison.Ordinal) == 0)
+            {
+                var indexOfEnd = input.IndexOf("]\n");
+                var index = "//[".Length;
+                if (index >= 0 && input.Length > index)
+                {
+                    delimiter = input.Substring(index, indexOfEnd - index);
+                }
+            }
+            else if (input.IndexOf("//") == 0)
+            {
+                delimiter = input.Substring(2, 1);
+            }
+
+            return delimiter;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorTest
+
+    {
+        [Test]
+        public void EmptyStringReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(""));
+        }
+
+        [Test]
+        public void BasicAddition()
+        {
+            Assert.AreEqual(3, StringCalculator.Add("1,2"));
+        }
+
+        [Test]
+        public void GetBasicDelimiter()
+        {
+            const string input = "//;\n1;2";
+            Assert.AreEqual(";", StringCalculator.GetDelimiter(input));
+        }
+
+        [Test]
+        public void OnePlusTwo()
+        {
+            var result = StringCalculator.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void OnePlusTwoPlus3()
+        {
+            var result = StringCalculator.Add("1,2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void OnePlusTwoWithNewLineNAndComma()
+        {
+            var result = StringCalculator.Add("1\n2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void OnePlusTwoWithNewLineRAndComma()
+        {
+            var result = StringCalculator.Add("1\r2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void OnePlusTwoWithNewLinesRandN()
+        {
+            var result = StringCalculator.Add("1\r2\n3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void OnePlusOneWithDelimiter()
+        {
+            var result = StringCalculator.Add("//;\n1;1");
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ExceptionMessageIsCorrectForOneNegative()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => StringCalculator.Add("-4,1"));
+            Assert.That(ex.Message, Is.EqualTo("Negatives are not allowed: -4 "));
+        }
+
+        [Test]
+        public void ExceptionMessageIsCorrectForThreeNegatives()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => StringCalculator.Add("1,2,3,1,-6,12,-3,34,-6"));
+            Assert.That(ex.Message, Is.EqualTo("Negatives are not allowed: -6 -3 -6 "));
+        }
+
+        [Test]
+        public void Adds1000()
+        {
+            var result = StringCalculator.Add("1,2,1000");
+            Assert.AreEqual(1003, result);
+        }
+
+        [Test]
+        public void IgnoresNumbersGreaterThan1000()
+        {
+            var result = StringCalculator.Add("1,1,1001,1,2345");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void RandomLengthDelimiter()
+        {
+            const string input = "//[***]\n1***2***3";
+            var result = StringCalculator.GetDelimiter(input);
+            Assert.AreEqual("***", result);
+        }
+
+        [Test]
+        public void DefaultDelimiter()
+        {
+            const string input = "1,2";
+            Assert.AreEqual("[,\n\r]", StringCalculator.GetDelimiter(input));
+        }
+    }
+}
